75ca152521a6701ebef4658b56c7883580c0bc5b
matiasderso <40612964+matiasderso@users.noreply.github.com>
2022-09-09 11:00:10 -0700

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-
 /**
  *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
  *Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int tens;
 	int ones;
 	int t;
 	int o;
-
 	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
 	{
 		for (ones = '0'; ones <= '9'; ones++)
@@ -38,6 +35,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
