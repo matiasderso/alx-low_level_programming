19103044df1034f67e283b17baa1502a7675750e
matiasderso <40612964+matiasderso@users.noreply.github.com>
2022-09-10 01:24:15 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,34 +5,31 @@
  */
 int main(void)
 {
-	int tens;
-	int ones;
-	int t;
-	int o;
-	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
-	{
-		for (ones = '0'; ones <= '9'; ones++)
-		{
-		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
-			{
-				for (o = ones + 1; o <= '9'; o++)
-				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(t);
-					putchar(o);
-					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				o = '0';
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+	int =0, n2;
+     While(n1 <= 99)
+{
+     n2 = n1;
+   while(n2 <= 99)
+  {
+       if(n2 != n1)
+{
+
+putchar((nl / 10) + 48);
+putchar((n1 % 10) + 48);
+putchar(' ');
+putchar ((n2 % 10) + 48)
+putchar ((n2 % 10) + 48)
+
+if(nl != 98 11 n2 != 99)
+{
+putchar(', ');
+putchar(', '); 
+}
+}
+++n2;
+}
+++n1;
+}
+putchar ('\n');
+return 0;
 }
