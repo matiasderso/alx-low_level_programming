9a4f79a1ddb449a2d9562c088338df2e8653589c
matiasderso <40612964+matiasderso@users.noreply.github.com>
2022-09-26 10:21:27 +0300

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,58 +1,30 @@
-#include "main.h
+#include "main.h"
 #include <stdio.h>
-
 /**
- *
- *  * _strchr - locates a character in a string .
- *
- *   * @s: string to check
- *
- *    * @c: character to found
- *
- *     * UPDATE V1.0 : Features : Pointers implementation instead of Arrays .
- *
- *      * Pointer/memory incrementation instead of i .
- *
- *       * Return: pointer to s or null
- *
- *        */
-
-
+ * _strchr - locates a character in a string .
+ * @s: string to check
+ * @c: character to found
+ * UPDATE V1.0 : Features : Pointers implementation instead of Arrays .
+ * Pointer/memory incrementation instead of i .
+ * Return: pointer to s or null
+ */
 
 char *_strchr(char *s, char c)
-
 {
 
+	while (*s)
+	{
+		s++;
+
+			if (*s == c)
+		{
+			return (s);
+		}
 
 
-		while (*s)
+	}
 
-				{
-
-							s++;
-
-
-
-										if (*s == c)
-
-													{
-
-																	return (s);
-
-																			}
-
-
-
-
-
-											}
-
-
-
-				return (0);
-
-
-
+		return (0);
 
 
 }
