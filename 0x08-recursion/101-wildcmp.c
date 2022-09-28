7c6fa5f9170f563e948a785af5b25df27c732ca9
matiasderso <40612964+matiasderso@users.noreply.github.com>
2022-09-28 14:29:44 +0300

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,3 +1,11 @@
+/**
+ * substring_match - check if a substring after wildcard matches s1
+ * @s1: one string
+ * @s2: one string
+ * @after_wldcd: placeholder for position right after wildcard
+ * Return: 1 if matched, 0 if not
+ */
+
 int substring_match(char *s1, char *s2, char *after_wldcd)
 {
 	if (*s1 == '\0' && *s2 == '\0')
@@ -13,6 +21,13 @@ int substring_match(char *s1, char *s2, char *after_wldcd)
 	else
 		return (substring_match(s1 + 1, after_wldcd, after_wldcd));
 }
+
+/**
+ * wildcmp - compare if string with wildcard mattches
+ * @s1: one string
+ * @s2: one string
+ * Return: 1 if matched, 0 if not
+ */
 
 int wildcmp(char *s1, char *s2)
 {
