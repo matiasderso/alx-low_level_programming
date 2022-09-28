7d80e0f032f113d46d0fd690590e5016231af29f
matiasderso <40612964+matiasderso@users.noreply.github.com>
2022-09-28 14:17:43 +0300

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,77 +1,26 @@
 #include "holberton.h"
 
-
-
-/**
-
- * helper - Finds the square root of two numbers
-
- *
-
- * @a: The number
-
- * @b: The number to test for the square root of @a
-
- *
-
- * Return: square root
-
- */
-
-
-
-int helper(int a, int b)
-
+int find_root(int n, int root)
 {
-
-	if (b * b > a)
-
+	if (root * root > n)
 		return (-1);
 
+	if (root * root == n)
+		return (root);
 
-
-	else if (b * b == a)
-
-		return (b);
-
-
-
-	else
-
-		return (helper(a, b + 1));
-
-
-
-	return (1);
-
+	return (find_root(n, root + 1));
 }
 
-
-
 /**
-
- * _sqrt_recursion - returns the natural square root of a number
-
- *
-
- * @n: the number to return the square root of
-
- *
-
- * Return: the square root of @n
-
+ * _sqrt_recursion - find natural square root of n
+ * @n: n
+ * Return: natural square root, or -1 if not natural root
  */
 
-
-
 int _sqrt_recursion(int n)
-
 {
+	if (n < 0)
+		return (-1);
 
-	if (n == 0)
-
-		return (0);
-
-	return (helper(n, 1));
-
+	return (find_root(n, 0));
 }
