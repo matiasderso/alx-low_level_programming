5624ef306f09220abe9c8a99d93973c0498b1094
matiasderso <40612964+matiasderso@users.noreply.github.com>
2022-09-28 14:20:35 +0300

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,103 +1,28 @@
-#include "holberton.h"
+#include "main.h"
 
-
-
-/**
-
- * palindromeChecker - checks the string
-
- *
-
- * @str: the string to be checked
-
- * @len: the length of the string
-
- * @i: the incrementor, starts at 0
-
- *
-
- * Return: 1 if it's a palindrome, 0 if it's not
-
- */
-
-
-
-int palindromeChecker(char *str, int len, int i)
-
+int compare(char *head, char *tail)
 {
 
-	if (i < len && str[i] == str[len])
-
-		return (palindromeChecker(str, len - 1, i + 1));
-
-	if (str[i] != str[len])
-
-		return (0);
-
-	return (1);
-
-}
-
-
-
-/**
-
- * _strlen_recursion - returns the length of a string
-
- *
-
- * @s: the string to check the length of
-
- *
-
- * Return: an integer that displays the length of the string
-
- */
-
-
-
-int _strlen_recursion(char *s)
-
-{
-
-	if (*s != '\0')
-
-		return (1 + _strlen_recursion(s + 1));
-
-
+	if (head >= tail)
+		return (1);
+	if (*head == *tail)
+		return (compare(head + 1, tail - 1));
 
 	return (0);
-
 }
 
-
-
-/**
-
- * is_palindrome - checks to see if a string is a palindrome
-
- *
-
- * @s: the string to check
-
- *
-
- * Return: 1 if it's a palindrome, 2 if it's not
-
- */
-
-
-
-int is_palindrome(char *s)
-
+int _strlen(char *s)
 {
 
-	int i = 0;
+	if (*s == '\0')
+		return (0);
+	s++;
+	return (1 + (_strlen(s)));
+}
 
-	int length = _strlen_recursion(s) - 1;
+int is_palindrome(char *s)
+{
+	int len = _strlen(s);
 
-
-
-	return (palindromeChecker(s, length, i));
-
+	return (compare(s, (s + len - 1)));
 }
